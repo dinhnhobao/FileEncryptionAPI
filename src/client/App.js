fb3835218d3f57f781a78fcd4171b6c5d00af38b
Dinh Nho Bao <nhobao2000@gmail.com>
2020-12-22 01:23:32 +0800

    Initial commit

---
+++ src/client/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './app.css';
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <div style={{fontWeight: 'bold'}}> Encrypt </div>
+        <div>
+          <form id="form-encrypt" action="http://localhost:3000/api/encrypt" method="post" encType="multipart/form-data">
+            File <input type="file" name="file" />
+          </form>
+        </div>
+        <div onClick={() => { document.getElementById("form-encrypt").submit() }}> Submit </div>
+        <br></br>
+        <div style={{fontWeight: 'bold'}}> Decrypt </div>
+        <div>
+          <form id="form-decrypt" action="http://localhost:3000/api/decrypt" method="post">
+            Filename <input type="text" name="filename" />
+            Password <input type="text" name="password" />
+          </form>
+        </div>
+        <div onClick={() => { document.getElementById("form-decrypt").submit() }}> Submit </div>
+      </div>
+    );
+  }
+}
