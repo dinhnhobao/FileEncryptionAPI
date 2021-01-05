99a63efe9bba501de9dc69a8387891ded895c1d8
Dinh Nho Bao <nhobao2000@gmail.com>
2021-01-06 03:27:59 +0800

    Enforce eslint google checkstyle
    https://www.npmjs.com/package/eslint

--- src/index.js
+++ src/index.js
@@ -1,154 +1,158 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
-const os = require('os');
 const Cryptify = require('cryptify');
 const app = express();
 const fs = require('fs');
 
-var utils = require('./utils');
+const utils = require('./utils');
 
 const STORAGE_PATH = './file';
 
 app.use(express.static('dist'));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true,
 }));
 app.use(bodyParser.json());
 
-var storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, STORAGE_PATH)
-    },
-    filename: function(req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname)
-    }
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, STORAGE_PATH);
+  },
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
 });
 
-var upload = multer({
-    storage: storage
+const upload = multer({
+  storage: storage,
 });
 
 app.get('/', upload.none(), (req, res) => {
-    res.redirect('/api/test');
+  res.redirect('/api/test');
 });
 
 app.get('/api/test', upload.none(), (req, res) => {
-    return res.json({
-        isWorking: true,
-    });
+  return res.json({
+    isWorking: true,
+  });
 });
 
 const deleteFile = (filePath) => {
-    fs.unlinkSync(filePath);
+  fs.unlinkSync(filePath);
 
-    try {
-        // remove tmp files
-        if (fs.existsSync(filePath + '.tmp')) {
-            fs.unlinkSync(filePath + '.tmp');
-        }
-    } catch (err) {
-        console.log("No .tmp file to delete");
+  try {
+    // remove tmp files
+    if (fs.existsSync(filePath + '.tmp')) {
+      fs.unlinkSync(filePath + '.tmp');
     }
-}
+  } catch (err) {
+    console.log('No .tmp file to delete');
+  }
+};
 
 app.post('/api/encrypt', upload.single('file'), (req, res) => {
-    const fileName = req.file.path.split("/")[1];
-    const filePath = STORAGE_PATH + '/' + fileName;
+  const fileName = req.file.path.split('/')[1];
+  const filePath = STORAGE_PATH + '/' + fileName;
 
-    const apiKey = req.headers.authorization;
-    if (!utils.isAuthorised(apiKey)) {
-        deleteFile(filePath);
-        return res.status(401).json({
-            message: "Wrong API access key. Please contact your adminstrator."
-        });
-    }
+  const apiKey = req.headers.authorization;
+  if (!utils.isAuthorised(apiKey)) {
+    deleteFile(filePath);
+    return res.status(401).json({
+      message: 'Wrong API access key. Please contact your adminstrator.',
+    });
+  }
 
-    const password = req.body.password;
-    if (!utils.isPasswordValid(password)) {
-        deleteFile(filePath);
-        return res.status(400).json({
-            message: "Your password is invalid. Please try again."
-        });
-    }
+  const password = req.body.password;
+  if (!utils.isPasswordValid(password)) {
+    deleteFile(filePath);
+    return res.status(400).json({
+      message: 'Your password is invalid. Please try again.',
+    });
+  }
 
-    const instance = new Cryptify(filePath, utils.parsePassword(password)); // depends on OS
+  const instance = new Cryptify(filePath,
+      utils.parsePassword(password)); // depends on OS
 
-    setTimeout(() => {
-        instance
-            .encrypt()
-            .catch(e => console.error(e));
-        console.log(`${filePath} encrypted`);
-    }, 1000);
+  setTimeout(() => {
+    instance
+        .encrypt()
+        .catch((e) => console.error(e));
+    console.log(`${filePath} encrypted`);
+  }, 1000);
 
-    setTimeout(() => {
-        console.log("File sending");
-        res.sendFile(fileName, {
-            root: STORAGE_PATH
-        }, (err) => {
-            if (err) {
-                console.log(err);
-                res.sendStatus(400);
-            }
-            deleteFile(filePath);
-        });
-    }, 5000);
+  setTimeout(() => {
+    console.log('File sending');
+    res.sendFile(fileName, {
+      root: STORAGE_PATH,
+    }, (err) => {
+      if (err) {
+        console.log(err);
+        res.sendStatus(400);
+      }
+      deleteFile(filePath);
+    });
+  }, 5000);
 });
 
 app.post('/api/decrypt', upload.single('file'), (req, res) => {
-    const fileName = req.file.path.split("/")[1];
-    const filePath = STORAGE_PATH + '/' + fileName;
+  const fileName = req.file.path.split('/')[1];
+  const filePath = STORAGE_PATH + '/' + fileName;
 
-    const apiKey = req.headers.authorization;
-    if (!utils.isAuthorised(apiKey)) {
-        deleteFile(filePath);
-        return res.status(401).json({
-            message: "Wrong API access key. Please contact your adminstrator."
+  const apiKey = req.headers.authorization;
+  if (!utils.isAuthorised(apiKey)) {
+    deleteFile(filePath);
+    return res.status(401).json({
+      message: 'Wrong API access key. Please contact your adminstrator.',
+    });
+  }
+
+  const password = req.body.password;
+  if (!utils.isPasswordValid(password)) {
+    deleteFile(filePath);
+    return res.status(400).json({
+      message: 'Your password is invalid. Please try again.',
+    });
+  }
+
+  const instance = new Cryptify(filePath,
+      utils.parsePassword(password)); // depends on OS
+
+  isSending = true;
+  setTimeout(() => {
+    instance
+        .decrypt()
+        .catch((e) => {
+          console.log('Error found');
+          console.error(e);
+          isSending = false;
         });
-    }
+    console.log(`${filePath} decrypted`);
+  }, 1000);
 
-    const password = req.body.password;
-    if (!utils.isPasswordValid(password)) {
-        deleteFile(filePath);
-        return res.status(400).json({
-            message: "Your password is invalid. Please try again."
-        });
-    }
-
-    const instance = new Cryptify(filePath, utils.parsePassword(password)); // depends on OS
-
-    isSending = true;
-    setTimeout(() => {
-        instance
-            .decrypt()
-            .catch(e => {
-                console.log("Error found");
-                console.error(e);
-                isSending = false;
-            });
-        console.log(`${filePath} decrypted`);
-    }, 1000);
-
-    setTimeout(() => {
-        if (isSending) {
-            console.log("File sending");
-            res.sendFile(fileName, {
-                root: STORAGE_PATH
-            }, (err) => {
-                if (err) {
-                    console.log(err);
-                    res.sendStatus(400);
-                }
-                deleteFile(filePath);
-            });
-        } else {
-            deleteFile(filePath);
-            return res.status(400).json({
-                message: "The password to decrypt the file is incorrect, or the file is corrupted. Please try again."
-            });
+  setTimeout(() => {
+    if (isSending) {
+      console.log('File sending');
+      res.sendFile(fileName, {
+        root: STORAGE_PATH,
+      }, (err) => {
+        if (err) {
+          console.log(err);
+          res.sendStatus(400);
         }
-    }, 5000);
+        deleteFile(filePath);
+      });
+    } else {
+      deleteFile(filePath);
+      return res.status(400).json({
+        message: 'The password to decrypt the file is incorrect,' +
+          ' ' + 'or the file is corrupted. Please try again.',
+      });
+    }
+  }, 5000);
 });
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Listening on port ${process.env.PORT || 8080}!`);
+});
