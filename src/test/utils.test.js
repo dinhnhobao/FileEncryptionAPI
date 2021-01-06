8b0d436d765fdd96be90e743ae5248c8cb54f830
Dinh Nho Bao <nhobao2000@gmail.com>
2021-01-06 16:59:48 +0800

    Change to correct assert method

--- src/test/utils.test.js
+++ src/test/utils.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+const assert = require('chai').assert;
 
 const { isAuthorised, isPasswordValid, parsePassword } = require('../main/utils');
 
@@ -6,34 +6,34 @@ const { SERVER_API_KEY, PASSWORD_INITIALS } = require('../main/constants');
 const { RUBBISH, EMPTY_PASSWORD, VALID_PASSWORD, 
     PASSWORD_WITH_SPECIAL_CHARS } = require('./constants');
 
-describe('API access key check', () => {
+describe('utils - API access key check', () => {
     it('returns true if the key is valid', () => {
-        assert(true, isAuthorised(SERVER_API_KEY));
+        assert.isTrue(isAuthorised(SERVER_API_KEY + ' .'));
     });
 
     it('returns false if the key is incorrect', () => {
-        assert.notStrictEqual(true, isAuthorised(SERVER_API_KEY + RUBBISH));
+        assert.isFalse(isAuthorised(SERVER_API_KEY + RUBBISH));
     });
 });
 
-describe('Password valid check', () => {
+describe('utils - Password valid check', () => {
     it('returns true if the password is valid', () => {
-        assert(true, isPasswordValid(VALID_PASSWORD));
+        assert.isTrue(isPasswordValid(VALID_PASSWORD));
     });
 
     it('returns false if the password is invalid', () => {
-        assert.notStrictEqual(true, isPasswordValid(EMPTY_PASSWORD));
+        assert.isTrue(isPasswordValid(EMPTY_PASSWORD));
     });
 });
 
-describe('Parse password check', () => {
+describe('utils - Parse password check', () => {
     it('password is parsed successfully for valid passwords', () => {
-        assert(PASSWORD_INITIALS + VALID_PASSWORD,
+        assert.strictEqual(PASSWORD_INITIALS + VALID_PASSWORD,
             parsePassword(VALID_PASSWORD));
     });
 
     it('password is parsed successfully for passwords with special chars', () => {
-        assert(PASSWORD_INITIALS + PASSWORD_WITH_SPECIAL_CHARS,
+        assert.strictEqual(PASSWORD_INITIALS + PASSWORD_WITH_SPECIAL_CHARS,
             parsePassword(PASSWORD_WITH_SPECIAL_CHARS));
     });
 
